6b14c9c257ab5c349a514339a1be656041ea9bbd
ammarkhan4244 <ammar.khan4244@gmail.com>
2026-06-14 16:31:48 +0500

    Add files via upload

---
+++ IndividualInformation.java
@@ -0,0 +1,11 @@
+public class IndividualInformation {
+
+    public static void main(String[] args) {
+
+        System.out.println("First Name: Ammar");
+        System.out.println("Last Name: Khan");
+        System.out.println("Street Address: 192/2, Street 15, Khayaban-E-Faisal, Phase 8, DHA");
+        System.out.println("City: Karachi");
+        System.out.println("Zip Code: 75950");
+    }
+}
